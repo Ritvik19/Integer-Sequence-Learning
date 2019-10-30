a37818bb33aa6046ba5be682e2eedd690e1351a5
Ritvik19 <rastogiritvik99@gmail.com>
2019-10-30 20:56:49 +0530

    Post 1

--- js/main.js
+++ js/main.js
@@ -1,34 +1,29 @@
-var c00= '#1f77b4', c01= '#ff7f0e', c02= '#2ca02c', c03= '#d62728', c04= '#9467bd';
-var c05= '#8c564b', c06= '#e377c2', c07= '#7f7f7f', c08= '#bcbd22', c09= '#17becf';
-var c10= '#aec7e8', c11= '#ffbb78', c12= '#98df8a', c13= '#ff9896', c14= '#c5b0d5';
-var c15= '#c49c94', c16= '#f7b6d2', c17= '#c7c7c7', c18= '#dbdb8d', c19= '#9edae5';
+function loadFile(filePath) {
+  var result = null;
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET", filePath, false);
+  xmlhttp.send();
+  if (xmlhttp.status==200) {
+    result = xmlhttp.responseText;
+  }
+  return result;
+}
 
 function plot(canvasid, plotdata) {
   var ctx = document.getElementById(canvasid);
   var myChart = new Chart(ctx, plotdata);
 }
-plotdata = {
-  type: 'bar',
-  data: {
-      labels: ["Android Studio", "Atom", "Coda", "Eclipse", "Emacs", "IPython / Jupyter", "IntelliJ", "Komodo", "Light Table", "NetBeans", "Notepad++", "PHPStorm", "PyCharm", "RStudio", "RubyMine", "Sublime Text", "TextMate", "Vim", "Visual Studio", "Visual Studio Code", "Xcode", "Zend"],
-      datasets: [{
-          label: 'Counts',
-          data: [14787, 11636, 578, 12591, 3922, 8317, 22166, 368, 178, 5121, 26621, 6645, 11724, 2940, 1202, 20424, 805, 22163, 27490, 44311, 8239, 307],
-          backgroundColor: c10,
-          borderColor: c00,
-          borderWidth: 1
-      },
-    ]
-  },
-  options: {
-      scales: {
-          yAxes: [{
-              ticks: {
-                  beginAtZero: true
-              }
-          }]
-      }
-  }
-}
 
-plot('c01', plotdata)
+
+main = document.getElementById('main')
+
+for (i=0; i < plotdata_.length; i++){
+  content = '<div class="w3-card-4 w3-margin w3-white">' +
+              '<div class="w3-container">' +
+                '<h3><b>'+titles[i]+'</b></h3>' +
+              '</div>' +
+              '<canvas id="c'+(i)+'"></canvas>' +
+            '</div>'
+  main.innerHTML += content
+  plot('c'+(i), plotdata_[i]);
+}
