e84376e5f69c5ba3cfd25dce20cce9bc0c6af28e
Ritvik19 <rastogiritvik99@gmail.com>
2019-10-30 21:34:00 +0530

    UI Improvements

--- js/main.js
+++ js/main.js
@@ -1,21 +1,10 @@
-function loadFile(filePath) {
-  var result = null;
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.open("GET", filePath, false);
-  xmlhttp.send();
-  if (xmlhttp.status==200) {
-    result = xmlhttp.responseText;
-  }
-  return result;
-}
-
 function plot(canvasid, plotdata) {
   var ctx = document.getElementById(canvasid);
   var myChart = new Chart(ctx, plotdata);
 }
 
-
 main = document.getElementById('main')
+postlist = document.getElementById('postlist')
 
 for (i=0; i < plotdata_.length; i++){
   content = '<div class="w3-card-4 w3-margin w3-white">' +
@@ -25,5 +14,6 @@ for (i=0; i < plotdata_.length; i++){
               '<canvas id="c'+(i)+'"></canvas>' +
             '</div>'
   main.innerHTML += content
+  postlist.innerHTML += '<li class="w3-padding-16"><span class="w3-large">'+titles[i]+'</span><br></li>'
   plot('c'+(i), plotdata_[i]);
 }
